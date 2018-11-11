fb72053e07940991fa4bef702e6c6e533fb96971
shunli.wang <shunli.wang@dwfund.net>
2018-11-11 19:33:01 +0800

    some modify about big num...

--- src/main.cpp
+++ src/main.cpp
@@ -3972,7 +3972,8 @@ bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& sta
     }
 
     // Check proof of stake
-    if (abs(block.baseTarget - getNextPotRequired(pindexPrev)) > 10)
+    int64_t abstemp = block.baseTarget - getNextPotRequired(pindexPrev);
+    if (abs(abstemp) > 10)
         return state.DoS(50, false, REJECT_INVALID, "bad-basetargetbits", false, "incorrect proof of tx");
 
     if (block.cumulativeDifficulty != GetNextCumulativeDifficulty(pindexPrev, block.baseTarget, consensusParams))
